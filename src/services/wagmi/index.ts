761feb8ff0e6e875f7b9d48307b068324d3a4892
awicone <awiconet@gmail.com>
2023-03-19 10:39:22 +0400

    test witn other wagmi config

--- src/services/wagmi/index.ts
+++ src/services/wagmi/index.ts
@@ -8,20 +8,44 @@ import {
   trustWallet,
   walletConnectWallet,
 } from '@rainbow-me/rainbowkit/wallets';
-import { configureChains, createClient, createStorage } from 'wagmi';
+import { Chain, configureChains, createClient, createStorage } from 'wagmi';
 import { bscTestnet } from 'wagmi/chains';
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 
 export const initialChain = bscTestnet;
 export const whitelistChains = [bscTestnet];
 
-const { provider, chains, webSocketProvider } = configureChains(whitelistChains, [
-  jsonRpcProvider({
-    rpc: (chain) => {
-      return { http: chain.rpcUrls.default.http[0] };
+const BSCChain: Chain = {
+  id: 97,
+  name: 'BSC Testnet',
+  network: 'bscTestnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Avalanche',
+    symbol: 'tBNB',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://muddy-damp-river.bsc-testnet.discover.quiknode.pro/c3b7c38b1462442ff398c0c9fc1450b8a157e76c/'],
     },
-  }),
-]);
+    public: {
+      http: ['https://muddy-damp-river.bsc-testnet.discover.quiknode.pro/c3b7c38b1462442ff398c0c9fc1450b8a157e76c/'],
+    }
+  },
+  blockExplorers: {
+    default: { name: 'BSC Scan', url: 'https://testnet.bscscan.com' },
+  },
+  testnet: true,
+};
+
+const { provider, chains, webSocketProvider} = configureChains(
+  [BSCChain],
+  [
+    jsonRpcProvider({
+      rpc: chain => ({ http: chain.rpcUrls.default.http[0] }),
+    }),
+  ]
+);
 
 // Rainbowkit connectors list
 const connectors = connectorsForWallets([

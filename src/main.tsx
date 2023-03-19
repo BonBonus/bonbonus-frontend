12c68d56068d1d764beb255c3db52667aa0e0778
awicone <awiconet@gmail.com>
2023-03-19 10:29:12 +0400

    add polyfill

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import '../polyfills'
 import '@rainbow-me/rainbowkit/styles.css';
 import 'react-toastify/dist/ReactToastify.css';
 import './assets/styles/index.scss';
